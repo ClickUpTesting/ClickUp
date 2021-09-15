ada0f7b158bcbef8262b1c9138f4c2dab48843fe
Jorge Caceres <jcaceresvelasco@gmail.com>
2021-09-15 10:45:27 -0400

    Add base api framework

--- src/main/java/core/api/ApiManager.java
+++ src/main/java/core/api/ApiManager.java
@@ -4,6 +4,8 @@
  * ("Confidential Information"). You shall not disclose such Confidential
  * Information and shall use it only in accordance with the terms of the
  * license agreement you entered into with JalaSoft
+ *
+ * @author Saul Caspa
  */
 
 package core.api;
