0f7c9a3adcb580ade5e2bdba31646879877aafed
Gustavo Huanca <gustavozha@gmail.com>
2021-10-13 11:46:21 -0400

    Fix checkstyle observation

--- src/main/java/clickup/entities/features/comment/Comment.java
+++ src/main/java/clickup/entities/features/comment/Comment.java
@@ -6,76 +6,76 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class Comment implements IFeature {
-	private long date;
-	@JsonProperty("hist_id")
-	private String histId;
-	private int id;
-	@JsonProperty("comment_text")
-	private String commentText;
-	private int assignee;
-	@JsonProperty("notify_all")
-	private boolean notifyAll;
+    private long date;
+    @JsonProperty("hist_id")
+    private String histId;
+    private int id;
+    @JsonProperty("comment_text")
+    private String commentText;
+    private int assignee;
+    @JsonProperty("notify_all")
+    private boolean notifyAll;
 
-	public void setDate(long date){
-		this.date = date;
-	}
+    public void setDate(long date) {
+        this.date = date;
+    }
 
-	public long getDate(){
-		return date;
-	}
+    public long getDate() {
+        return date;
+    }
 
-	public void setHistId(String histId){
-		this.histId = histId;
-	}
+    public void setHistId(String histId) {
+        this.histId = histId;
+    }
 
-	public String getHistId(){
-		return histId;
-	}
+    public String getHistId() {
+        return histId;
+    }
 
-	public void setId(int id){
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public int getId(){
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getCommentText() {
-		return commentText;
-	}
+    public String getCommentText() {
+        return commentText;
+    }
 
-	public void setCommentText(String commentText) {
-		this.commentText = commentText;
-	}
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
 
-	public int getAssignee() {
-		return assignee;
-	}
+    public int getAssignee() {
+        return assignee;
+    }
 
-	public void setAssignee(int assignee) {
-		this.assignee = assignee;
-	}
+    public void setAssignee(int assignee) {
+        this.assignee = assignee;
+    }
 
-	public boolean isNotifyAll() {
-		return notifyAll;
-	}
+    public boolean isNotifyAll() {
+        return notifyAll;
+    }
 
-	public void setNotifyAll(boolean notifyAll) {
-		this.notifyAll = notifyAll;
-	}
+    public void setNotifyAll(boolean notifyAll) {
+        this.notifyAll = notifyAll;
+    }
 
-	@Override
-	public String getIdentifier() {
-		return String.valueOf(id);
-	}
+    @Override
+    public String getIdentifier() {
+        return String.valueOf(id);
+    }
 
-	@Override
-	public String getRequiredField() {
-		return null;
-	}
+    @Override
+    public String getRequiredField() {
+        return null;
+    }
 
-	@Override
-	public void setDefaultValues() {
+    @Override
+    public void setDefaultValues() {
 
-	}
+    }
 }
