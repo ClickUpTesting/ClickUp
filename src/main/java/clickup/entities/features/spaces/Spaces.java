b940d34d7e0794c94d4fd85b8a6fa18c4c872035
Raymundo Guaraguara <rguaraguara@Raymundos-MacBook-Pro.local>
2021-10-15 17:45:40 -0400

    add copyright

--- src/main/java/clickup/entities/features/spaces/Spaces.java
+++ src/main/java/clickup/entities/features/spaces/Spaces.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2021 JalaSoft.
+ * This software is the confidential and proprietary information of JalaSoft
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with JalaSoft
+ *
+ * @author Raymundo Guaraguara
+ */
+
 package clickup.entities.features.spaces;
 
 import clickup.entities.features.IGetAllFeatures;
