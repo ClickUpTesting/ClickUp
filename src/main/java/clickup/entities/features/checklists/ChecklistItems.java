076fad48fea612f50c78f25d01d4eb806fbc97a8
Jorge Caceres <jcaceresvelasco@gmail.com>
2021-10-21 12:07:40 -0400

    Add step to verify checklist item inside a checklist

--- src/main/java/clickup/entities/features/checklists/ChecklistItems.java
+++ src/main/java/clickup/entities/features/checklists/ChecklistItems.java
@@ -13,8 +13,11 @@ package clickup.entities.features.checklists;
 import clickup.entities.features.IFeature;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static core.api.ApiRequestSpecificationProvider.add;
 
 public class ChecklistItems implements IFeature {
     private String id;
@@ -117,8 +120,18 @@ public class ChecklistItems implements IFeature {
 
     }
 
+    /**
+     * Creates a map with the values set on the scenario.
+     *
+     * @param body with the fields set on the scenario
+     * @return valuesMap corresponding feature
+     * @author Jorge Caceres
+     */
     @Override
     public Map<String, String> getMatchedValues(IFeature featureResponse, Map<String, String> body) {
-        return null;
+        ChecklistItems checklistItems = (ChecklistItems) featureResponse;
+        Map<String, String> valuesMap = new HashMap<>();
+        add(() -> valuesMap.put("name", checklistItems.getName()), () -> body.get("name"));
+        return valuesMap;
     }
 }
--- src/test/resources/features/checklist/create_checklist_item.feature
+++ src/test/resources/features/checklist/create_checklist_item.feature
@@ -1,10 +1,11 @@
 Feature: Create checklist item
 
   @API @SmokeTesting @DeleteChecklistItem
-  Scenario: As a user I want to create a checklist item
-    Given I set the checklistItem with request endpoint to /checklist/{checklist_id}/checklist_item
+  Scenario: As a user I want to create a checklist item inside a checklist
+    Given I set the checklists with request endpoint to /checklist/{checklist_id}/checklist_item
     When I set the request body with following values:
       | name | My checklist item |
-      And I execute a POST request
-    Then I verify that the checklist item response status is 200
+      And I execute the POST request
+    Then I verify that the response status is 200
+      And I verify the checklist item body inside the checklist
       And I verify the schema matches the file: schemas/checklist/create_checklist.json
