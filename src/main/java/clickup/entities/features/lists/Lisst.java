25c28f4d15f6803b3dce16ec9f52664d69782253
Gustavo Huanca <gustavozha@gmail.com>
2021-10-21 09:00:14 -0400

    Delete blank line

--- src/main/java/clickup/entities/features/lists/Lisst.java
+++ src/main/java/clickup/entities/features/lists/Lisst.java
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
