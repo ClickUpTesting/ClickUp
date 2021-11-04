026b2eb651c07bb6bc7d92c3db0ff1c53496e5e4
Gustavo Huanca <gustavozha@gmail.com>
2021-11-04 17:41:02 -0400

    Add  copyrigth

--- src/test/java/cucumber/ui/steps/TaskSteps.java
+++ src/test/java/cucumber/ui/steps/TaskSteps.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2021 JalaSoft.
+ * This software is the confidential and proprietary information of JalaSoft
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with JalaSoft
+ *
+ * @author Gustavo Huanca
+ */
+
 package cucumber.ui.steps;
 
 import clickup.ui.pages.ListPage;
@@ -11,7 +21,6 @@ import core.selenium.WebDriverManager;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.testng.asserts.SoftAssert;
-
 import java.util.Map;
 
 public class TaskSteps {
