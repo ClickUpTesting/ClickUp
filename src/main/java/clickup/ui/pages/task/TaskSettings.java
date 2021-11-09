19a32fe8dbd0b3ee3105cf5cea069e8ca58be255
Gustavo Huanca <gustavozha@gmail.com>
2021-11-09 17:29:19 -0400

    Add java doc

--- src/main/java/clickup/ui/pages/task/TaskSettings.java
+++ src/main/java/clickup/ui/pages/task/TaskSettings.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2021 JalaSoft.
+ * This software is the confidential and proprietary information of JalaSoft
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with JalaSoft
+ *
+ * @author Gustavo Huanca
+ */
+
 package clickup.ui.pages.task;
 
 import clickup.ui.pages.BasePage;
@@ -13,12 +23,22 @@ public class TaskSettings extends BasePage {
         super(webDriverManager);
     }
 
+    /**
+     * Clicks in Delete icon
+     *
+     * @author Gustavo Huanca
+     */
     public void clickDeleteIcon() {
         webDriverActions.clickElement(deleteIcon);
     }
 
+    /**
+     * Waits for the page to be loaded.
+     *
+     * @author Gustavo Huanca
+     */
     @Override
     protected void waitForPageLoaded() {
-
+        webDriverWaits.waitVisibilityOfElement(deleteIcon);
     }
 }
