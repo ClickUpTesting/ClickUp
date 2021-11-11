0abb008187b53a63d6752b8bbcb4d055127e2148
Gustavo Huanca <gustavozha@gmail.com>
2021-11-11 17:40:37 -0400

    add  fiels and methods to create a formula

---
+++ src/main/java/clickup/ui/pages/list/FormulaForm.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2021 JalaSoft.
+ * This software is the confidential and proprietary information of JalaSoft
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with JalaSoft
+ *
+ * @author Gustavo Huanca
+ */
+
+package clickup.ui.pages.list;
+
+import core.selenium.WebDriverManager;
+
+public class FormulaForm extends RatingForm {
+    public FormulaForm(WebDriverManager webDriverManager) {
+        super(webDriverManager);
+    }
+}
