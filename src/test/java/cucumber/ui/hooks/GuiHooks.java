6a58bd8a6928374e962024c838d97f3915fbd47f
Gustavo Huanca <gustavozha@gmail.com>
2021-11-11 14:04:59 -0400

    Fix checkstyle observation

--- src/test/java/cucumber/ui/hooks/GuiHooks.java
+++ src/test/java/cucumber/ui/hooks/GuiHooks.java
@@ -10,8 +10,6 @@
 
 package cucumber.ui.hooks;
 
-import clickup.ui.pages.LoginPage;
-import clickup.ui.utils.PageTransporter;
 import core.api.ApiHeaders;
 import core.api.ApiRequestBuilder;
 import core.selenium.WebDriverManager;
@@ -19,20 +17,16 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.testng.asserts.SoftAssert;
 
-import static clickup.ui.utils.PageUrl.getUrlPage;
-
 public class GuiHooks {
     private ApiRequestBuilder apiRequestBuilder;
     private SoftAssert softAssert;
     private WebDriverManager webDriverManager;
-    private PageTransporter pageTransporter;
 
     public GuiHooks(final ApiRequestBuilder apiRequestBuilder, final SoftAssert softAssert,
                     final WebDriverManager webDriverManager) {
         this.apiRequestBuilder = apiRequestBuilder;
         this.softAssert = softAssert;
         this.webDriverManager = webDriverManager;
-        this.pageTransporter = new PageTransporter(webDriverManager);
     }
 
     @Before(order = 1, value = "@GUI")
